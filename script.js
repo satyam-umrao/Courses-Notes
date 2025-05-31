bca61a4d37b6cebd4336cbc2eb447ed5da4bc3af
Satyam Umrao <satyamumrao11225@gmail.com>
2025-05-31 09:37:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-   const hamburger = document.getElementById('hamburger');
+// Copyright (c) 2025 Satyam Umrao
+// All rights reserved. Unauthorized copying or modification is strictly prohibited.
+
+const hamburger = document.getElementById('hamburger');
   const navLinks = document.getElementById('navLinks');
 
   hamburger.addEventListener('click', () => {
